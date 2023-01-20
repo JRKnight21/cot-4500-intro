4c99f1545d089536a2a93b89e2b076392682664c
JRKnight21 <joannaromano@Knights.ucf.edu>
2023-01-19 22:05:29 -0500

    1.19.23 Initial Push to establish repo.

---
+++ src/main/main.py
@@ -0,0 +1,12 @@
+def matrix1():
+    matrix = [[1 if i == j else 0 for j in range(3)] for i in range(3)]
+    for row in matrix:
+        print(row)
+
+def matrix2():
+    matrix = [[1 if i==j else 0 for j in range(3)] for i in range(3)]
+    for row in matrix:
+        print(row)
+
+if __name__ == "__main__":
+    matrix1()
