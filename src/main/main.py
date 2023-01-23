926ff5fd7bb31b8c68eb69d0b56e2b1815b55740
JRKnight21 <joannaromano@Knights.ucf.edu>
2023-01-22 19:12:57 -0500

    Updated code.

--- src/main/main.py
+++ src/main/main.py
@@ -1,18 +1,42 @@
-def matrix1():
-    matrix = [[1 if i == j else 0 for j in range(3)] for i in range(3)]
-    for row in matrix:
-        print(row)
+import numpy as np
 
-#def matrix2():
-#   matrix_size = range(3)
-#    i_equals_j = i==j
-#
-#    matrix = [[1 if i == j else 0 for j in matrix_size] for i in matrix_size]
-#    for row in matrix:
-#        print(row)
+# answer 1
+#function to call first matrix
+def create_matrix():
+    # indices i and j
+    i, j = np.indices((3,3))
+    # if i==j then 1, else 0
+    matrix = np.where(i == j, 1, 0)
+    # flattens matrix into one line
+    # this helps remove the brackets when printing
+    matrix = matrix.flatten()
+
+    # takes the flat line matrix and makes it back into a 3x3
+    for i in range(0, len(matrix), 3):
+        print(*matrix[i:i+3])
+    # creates a new line between problems
+    print()
+
+def modify_matrix():
+    i, j = np.indices((3,3))
+    transformed_matrix = np.where(i != j, + 3, 1)
+    transformed_matrix = transformed_matrix.flatten()
+
+    for i in range(0, len(transformed_matrix), 3):
+        print(*transformed_matrix[i:i+3])
+    print()
+
+def delete_last_col():
+    i, j = np.indices((3,3))
+    truncated_matrix = np.where(i != j, + 3, 1)
+    truncated_matrix = np.delete(truncated_matrix, -1, axis=1)
+    truncated_matrix = truncated_matrix.flatten()
+    
+    for i in range(0, len(truncated_matrix), 2):
+        print(*truncated_matrix[i:i+2])
 
 
 if __name__ == "__main__":
-    matrix1()
-    #matrix2()
-    #matrix3()
+    create_matrix()
+    modify_matrix()
+    delete_last_col()
