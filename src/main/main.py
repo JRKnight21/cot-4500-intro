bd4b6d823b87d29122087d6ab72fd5c5fafbac31
JRKnight21 <joannaromano@Knights.ucf.edu>
2023-01-22 20:20:32 -0500

    Updated comments.

--- src/main/main.py
+++ src/main/main.py
@@ -1,39 +1,46 @@
 import numpy as np
 
-# answer 1
-#function to call first matrix
+# Answer 1:
+# function to call the first matrix
 def create_matrix():
-    # indices i and j
+#   indices i and j
     i, j = np.indices((3,3))
-    # if i==j then 1, else 0
+#   if i==j then 1, else 0
     matrix = np.where(i == j, 1, 0)
-    # flattens matrix into one line
-    # this helps remove the brackets when printing
+#   this helps remove the brackets when printing
+#   this flattens matrix into one line (1D)
     matrix = matrix.flatten()
 
-    # takes the flat line matrix and makes it back into a 3x3
-    for i in range(0, len(matrix), 3):
-        print(*matrix[i:i+3])
-    # creates a new line between problems
-    print()
+#   uses print_matrix function
+#   recreates a 3D matrix with the propper dimensions (3x3)
+    print_matrix(matrix, 0, len(matrix), 3)
 
+#   Answer 2:
 def modify_matrix():
     i, j = np.indices((3,3))
+#   Adds 3 to every cell where i != j
     transformed_matrix = np.where(i != j, + 3, 1)
     transformed_matrix = transformed_matrix.flatten()
 
-    for i in range(0, len(transformed_matrix), 3):
-        print(*transformed_matrix[i:i+3])
-    print()
+    print_matrix(transformed_matrix, 0, len(transformed_matrix), 3)
 
+#   Answer 3:
 def delete_last_col():
     i, j = np.indices((3,3))
+#   Same matrix as #2
     truncated_matrix = np.where(i != j, + 3, 1)
+#   Deletes last column to make a 3x2 matrix
     truncated_matrix = np.delete(truncated_matrix, -1, axis=1)
     truncated_matrix = truncated_matrix.flatten()
     
-    for i in range(0, len(truncated_matrix), 2):
-        print(*truncated_matrix[i:i+2])
+    print_matrix(truncated_matrix, 0, len(truncated_matrix), 2)
+
+#   takes a flattend 1D matrix and makes it the proper dimensions  
+def print_matrix(input ,start, stop, step):
+    for i in range(start, stop, step):
+        print(*input[i:i+step])
+
+    print()
 
 
 if __name__ == "__main__":
